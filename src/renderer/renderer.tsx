e3aa643f5e5d758a7aaf5056cdd5ac685f1f7b0e
Ilyacnt <ilyagt2012@gmail.com>
2023-06-25 15:09:13 +0300

    React Router Dom setup

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import Layout from './layout/Layout'
 
-const Layout = () => {
-    return <h1>Hello World</h1>
-}
+const App = () => (
+    <MemoryRouter>
+        <Routes>
+            <Route path="/" element={<Layout />} />
+        </Routes>
+    </MemoryRouter>
+)
 
 const root = createRoot(document.getElementById('root') as Element)
-root.render(<Layout />)
+root.render(<App />)
