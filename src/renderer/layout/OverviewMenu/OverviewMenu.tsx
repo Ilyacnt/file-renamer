f6e6ad0b14ffb1652dc98b682d52e04add79ede6
Ilyacnt <ilyagt2012@gmail.com>
2023-06-26 13:26:48 +0300

    OverviewMenu component

--- src/renderer/layout/OverviewMenu/OverviewMenu.tsx
+++ src/renderer/layout/OverviewMenu/OverviewMenu.tsx
@@ -1,11 +1,32 @@
 import styles from './OverviewMenu.module.css'
-import CopyIcon from '@/assets/copy.svg'
+import FilesIcon from '@/assets/copy.svg'
+import NamingsIcon from '@/assets/edit.svg'
+import { useState } from 'react'
+import { CurrentPage } from '../../types/pages'
+import cn from 'classnames'
 
 const OverviewMenu = () => {
+    const [currentPage, setCurrentPage] = useState<CurrentPage>('files')
+
+    const changeCurrentPageHandler = (page: CurrentPage) => {
+        setCurrentPage(page)
+    }
+
     return (
         <div className={styles.OverviewMenu}>
-            <div className={styles.Item}>
-                <CopyIcon />
+            <div
+                className={cn(styles.Item, { [styles.Selected]: currentPage === 'files' })}
+                onClick={() => changeCurrentPageHandler('files')}
+            >
+                <FilesIcon />
+                <p>Files</p>
+            </div>
+            <div
+                className={cn(styles.Item, { [styles.Selected]: currentPage === 'namings' })}
+                onClick={() => changeCurrentPageHandler('namings')}
+            >
+                <NamingsIcon />
+                <p>Namings</p>
             </div>
         </div>
     )
