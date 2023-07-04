58fc1b3318135c462210e60d6634fc55dbfe82d6
Ilyacnt <ilyagt2012@gmail.com>
2023-07-04 22:08:40 +0300

    Namings type added

---
+++ src/renderer/types/naming.d.ts
@@ -0,0 +1,15 @@
+export interface NamingItem {
+    name: string
+    description: string
+    constructorProperties: CounsctructorProperty[]
+}
+
+export interface CounsctructorProperty {
+    name: string
+    type: CounsctructorPropertyTypes
+    currentValue: string | null
+    additionalData?: string
+}
+
+export type CounsctructorPropertyTypes = 'selectData' | 'uniqueCode' | 'simpleText' | 'resolution'
+export type ConstructorPropertyValuesDataTypes = 'googleSheets' | 'plainText'
